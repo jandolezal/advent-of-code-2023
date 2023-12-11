b55a369c63b1387a3b06489fef28952f3094e274
Jan Doležal <jandolezal@email.cz>
2023-12-11 16:03:44 +0100

    part 2 for day 10 pipe maze

--- 10/pipe_maze.py
+++ 10/pipe_maze.py
@@ -21,6 +21,20 @@ L|7||
 L|-JF
 """
 
+
+test_input3 = """OF----7F7F7F7F-7OOOO
+O|F--7||||||||FJOOOO
+O||OFJ||||||||L7OOOO
+FJL7L7LJLJ||LJIL-7OO
+L--JOL7IIILJS7F-7L7O
+OOOOF-JIIF7FJ|L7L7L7
+OOOOL7IF7||L7|IL7L7|
+OOOOO|FJLJ|FJ|F7|OLJ
+OOOOFJL-7O||O||||OOO
+OOOOL---JOLJOLJLJOOO
+"""
+
+
 position = namedtuple("Position", ["pipe", "r", "c", "origin"])
 
 options = {
@@ -92,6 +106,54 @@ def part1(data, n_rows, n_cols):
             return positions, steps // 2
 
 
+# i am cheating with this one
+# found the even-odd rule algorithm mentioned on reddit and used it as is
+# https://www.reddit.com/r/adventofcode/comments/18evyu9/2023_day_10_solutions/
+# takes approx. 20 seconds to run
+# https://en.wikipedia.org/wiki/Even%E2%80%93odd_rule
+def is_point_in_path(x: int, y: int, poly: list[tuple[int, int]]) -> bool:
+    """Determine if the point is on the path, corner, or boundary of the polygon
+
+    Args:
+      x -- The x coordinates of point.
+      y -- The y coordinates of point.
+      poly -- a list of tuples [(x, y), (x, y), ...]
+
+    Returns:
+      True if the point is in the path or is a corner or on the boundary"""
+    num = len(poly)
+    j = num - 1
+    c = False
+    for i in range(num):
+        if (x == poly[i][0]) and (y == poly[i][1]):
+            # point is a corner
+            return True
+        if (poly[i][1] > y) != (poly[j][1] > y):
+            slope = (x - poly[i][0]) * (poly[j][1] - poly[i][1]) - (
+                poly[j][0] - poly[i][0]
+            ) * (y - poly[i][1])
+            if slope == 0:
+                # point is on boundary
+                return True
+            if (slope < 0) != (poly[j][1] < poly[i][1]):
+                c = not c
+        j = i
+    return c
+
+
+def part2(input, positions):
+    lines = input.splitlines()
+    positions = [(position.r, position.c) for position in positions]
+
+    count = 0
+    for i, line in enumerate(lines):
+        for j, col in enumerate(line):
+            if is_point_in_path(i, j, positions):
+                count += 1
+
+    return count - len(positions)
+
+
 with open("10/input.txt") as f:
     input = f.read()
 
@@ -109,3 +171,16 @@ assert test_result2 == 4, f"Test result in part 1 loop should be 4, not {test_re
 data1, n_rows, n_cols = prepare_data(input)
 _, result1 = part1(data1, n_rows, n_cols)
 print(result1)  # 7012
+
+
+# part 2
+test_data3, n_rows, n_cols = prepare_data(test_input3)
+positions, _ = part1(test_data3, n_rows, n_cols)
+test_result3 = part2(test_input3, positions)
+assert test_result3 == 8, f"Test result should be 8, not {test_result3}"
+
+
+data2, n_rows, n_cols = prepare_data(input)
+positions, _ = part1(data2, n_rows, n_cols)
+result2 = part2(input, positions)
+print(result2)  # 395
